51db57db6921ff61da8d7f90f2cf255b5d55dde4
rikopernando <riko.pernando@bareksa.com>
2020-02-05 08:31:47 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+
+const SendIcon = () => (
+  <svg 
+    style={{marginTop: 7, fontSize: 25, marginLeft: 10}}
+    width="1em" 
+    height="1em" 
+    viewBox="0 0 24 24">
+      <path 
+        d="M4 6.03l7.5 3.22l-7.5-1V6.03m7.5 8.72L4 17.97v-2.22l7.5-1M2 3v7l15 2l-15 2v7l21-9L2 3z"
+        fill="#554fe8"/>
+  </svg>
+)
+
+const ChatHeader = styled.div`
+  color: #ffffff;
+  margin: 12px auto;
+  font-weight: bold;
+`
+
+const ChatBox = styled.div`
+  padding: 10px;
+  font-size: 13px;
+`
+
+const ChatHandleBox = styled.div`
+  max-width: 480px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  background-color: #ffffff; 
+  margin: 0 auto;
+  left: 50%;
+  transform: translate(-50%);
+  border-top: 2px solid #ece9e9;
+  padding: 5px 10px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const ChatHandle = styled.textarea`
+  font-size: 12px;
+  padding: 5px;
+  width: 100%;
+  border-color: transparent;
+  letter-spacing: 1px;
+
+  :focus {
+    outline: none;
+  }
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+      <div className="container">
+        <ChatHeader>
+          header chat
+        </ChatHeader>
+      </div>
+    </header>
+    <main>
+      <ChatBox>
+        Chat App
+      </ChatBox>
+      <ChatHandleBox>
+        <ChatHandle 
+          rows="2" 
+          cols="20"
+          placeholder="Type a message"
+          type="text" />
+        <SendIcon/>
+      </ChatHandleBox>
+    </main>
+    </>
   );
 }
 
