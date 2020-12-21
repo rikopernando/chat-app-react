4b67971ec6eab1fdae58c1a69c2503b09b60e2a2
rikopernando <rikopernando96@gmail.com>
2020-12-21 21:47:15 +0700

    add eslint and prettier

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,29 +3,22 @@ import {Switch, BrowserRouter as Router} from "react-router-dom"
 import PublicRoute from "./PublicRouter"
 import PrivateRoute from "./PrivateRouter"
 
-const HomePage = lazy(() => import('../pages/'))
-const ChatPage = lazy(() => import('../pages/chat'))
+const HomePage = lazy(() => import("../pages/"))
+const ChatPage = lazy(() => import("../pages/chat"))
 
-const AppRouter = () => {
-  return (
-    <Fragment>
-      <Suspense fallback={null}>
-        <main>
-          <Router>
-            <Switch>
-              <PublicRoute 
-                path="/" 
-                component={HomePage} 
-                exact />
-              <PrivateRoute 
-                path="/chat" 
-                component={ChatPage} />
-            </Switch>
-          </Router>
-        </main>
-      </Suspense>
-    </Fragment>
-  )
-}
+const AppRouter = () => (
+  <Fragment>
+    <Suspense fallback={null}>
+      <main>
+        <Router>
+          <Switch>
+            <PublicRoute path="/" component={HomePage} exact />
+            <PrivateRoute path="/chat" component={ChatPage} />
+          </Switch>
+        </Router>
+      </main>
+    </Suspense>
+  </Fragment>
+)
 
 export default AppRouter
