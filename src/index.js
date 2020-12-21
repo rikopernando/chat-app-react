83c6bd0fe6cb9f09e1f7f6e7b933be88b4566a90
rikopernando <rikopernando96@gmail.com>
2020-12-21 21:43:22 +0700

    add eslint and prettier

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import AppRouter from './routers/AppRouter'
-import configureStore from './store/configureStore'
-import * as serviceWorker from './serviceWorker'
-import './styles/styles.scss'
+import React from "react"
+import ReactDOM from "react-dom"
+import {Provider} from "react-redux"
+import AppRouter from "./routers/AppRouter"
+import configureStore from "./store/configureStore"
+import "./styles/styles.scss"
 
 const store = configureStore()
 
 const app = (
-  <Provider store={store} >
-    <AppRouter/>
-  </Provider>
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
 )
 
-ReactDOM.render(app, document.getElementById('root'))
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(app, document.getElementById("root"))
